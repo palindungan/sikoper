c44ae624f6d301c6812891bef0321909d4cab890
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-09-12 14:29:54 +0700

    design form
    design form

---
+++ application/controllers/admin/Home.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+
+    public function index()
+    {
+        $this->template->load('notika/view_1', 'notika/konten/form/v_form');
+    }
+}
