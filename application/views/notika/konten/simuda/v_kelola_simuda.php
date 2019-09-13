1cc2141b0a3a93c6a85cbc1c9c10f6f1570d279a
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-09-13 12:37:06 +0700

    Update v_kelola_simuda.php

--- application/views/notika/konten/simuda/v_kelola_simuda.php
+++ application/views/notika/konten/simuda/v_kelola_simuda.php
@@ -10,43 +10,43 @@
                  <div class="form-element-list">
 
                      <div class="row">
-                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                             <div class="form-group ic-cmp-int">
-                                 <div class="form-ic-cmp">
-                                     <i class="fa fa-list-alt"></i>
-                                 </div>
-                                 <div class="nk-int-st">
-                                     <input type="text" class="form-control" placeholder="Nama Produk">
-                                 </div>
-                             </div>
-                         </div>
-                     </div>
-                     <div class="row">
                          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                              <div class="form-group ic-cmp-int">
                                  <div class="form-ic-cmp">
                                      <i class="fa fa-truck"></i>
                                  </div>
                                  <div class="nk-int-st">
-                                     <input type="number" class="form-control" placeholder="Berat">
+                                     <div class="bootstrap-select fm-cmp-mg">
+                                         <select class="selectpicker" data-live-search="true">
+                                             <option value="">No Rekening (Anggota)</option>
+                                             <option>091231273 (Budi)</option>
+                                             <option>091237127 (Yadi)</option>
+                                             <option>016622892 (Slamet)</option>
+                                             <option>078126772 (Joko)</option>
+                                         </select>
+                                     </div>
                                  </div>
                              </div>
                          </div>
+
                          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                              <div class="form-group ic-cmp-int">
                                  <div class="form-ic-cmp">
-                                     <i class="fa fa-tag"></i>
+                                     <i class="fa fa-truck"></i>
                                  </div>
-                                 <div class="bootstrap-select fm-cmp-mg">
-                                     <select class="selectpicker">
-                                         <option value="">Pilih Satuan Berat</option>
-                                         <option>mg</option>
-                                         <option>gr</option>
-                                         <option>kg</option>
-                                     </select>
+                                 <div class="nk-int-st">
+                                     <div class="bootstrap-select fm-cmp-mg">
+                                         <select class="selectpicker">
+                                             <option value="">Jenis Simuda</option>
+                                             <option>Debit</option>
+                                             <option>Kredit</option>
+                                         </select>
+                                     </div>
                                  </div>
                              </div>
                          </div>
+
+
                      </div>
                      <div class="row">
                          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
@@ -55,39 +55,50 @@
                                      <i class="fa fa-money"></i>
                                  </div>
                                  <div class="nk-int-st">
-                                     <input type="number" class="form-control" placeholder="Harga">
+                                     <input type="number" class="form-control" placeholder="   Saldo Awal">
                                  </div>
                              </div>
                          </div>
                          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                              <div class="form-group ic-cmp-int">
                                  <div class="form-ic-cmp">
-                                     <i class="fa fa-th"></i>
+                                     <i class="fa fa-money"></i>
                                  </div>
                                  <div class="nk-int-st">
-                                     <div class="bootstrap-select fm-cmp-mg">
-                                         <select class="selectpicker">
-                                             <option value="">Pilih Status Stok</option>
-                                             <option>Tersedia</option>
-                                             <option>Kosong</option>
-                                         </select>
+                                     <input type="number" class="form-control" placeholder="   Saldo Akhir">
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+
+                     <div class="row">
+
+                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                             <div class="form-group ic-cmp-int">
+                                 <div class="form-ic-cmp">
+                                     <i class="fa fa-tag"></i>
+                                 </div>
+                                 <div class="form-group nk-datapk-ctm form-elet-mg" id="data_1">
+                                     <div class="input-group date nk-int-st">
+                                         <span class="input-group-addon"></span>
+                                         <input type="text" class="form-control" placeholder="   Tanggal">
                                      </div>
                                  </div>
                              </div>
                          </div>
-                     </div>
-                     <div class="row">
-                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                             <div class="form-group ic-cmp-int form-elet-mg res-mg-fcs">
+
+                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                             <div class="form-group ic-cmp-int">
                                  <div class="form-ic-cmp">
-                                     <i class="fa fa-arrow-right"></i>
+                                     <i class="fa fa-money"></i>
                                  </div>
                                  <div class="nk-int-st">
-                                     <textarea name="" id="" rows="5" class="form-control" placeholder="Deskripsi Produk"></textarea>
+                                     <input type="number" class="form-control" placeholder="   Jumlah">
                                  </div>
                              </div>
                          </div>
                      </div>
+
 
                      <br>
 
